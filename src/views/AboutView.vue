980900200d17a273a88cea99aa3e0e96a11af880
Batuhan <batuhan.fidan@ogr.iu.edu.tr>
2022-08-13 01:51:49 +0300

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo aspernatur, illum laboriosam sit neque accusamus fugit ab ullam vero tempora dicta aperiam itaque odio quod maxime at totam blanditiis cumque!  
+    </p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo aspernatur, illum laboriosam sit neque accusamus fugit ab ullam vero tempora dicta aperiam itaque odio quod maxime at totam blanditiis cumque!  
+    </p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo aspernatur, illum laboriosam sit neque accusamus fugit ab ullam vero tempora dicta aperiam itaque odio quod maxime at totam blanditiis cumque!  
+    </p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo aspernatur, illum laboriosam sit neque accusamus fugit ab ullam vero tempora dicta aperiam itaque odio quod maxime at totam blanditiis cumque!  
+    </p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo aspernatur, illum laboriosam sit neque accusamus fugit ab ullam vero tempora dicta aperiam itaque odio quod maxime at totam blanditiis cumque!  
+    </p>
   </div>
 </template>
